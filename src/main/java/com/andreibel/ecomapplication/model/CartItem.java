640fcb47c17d4c13d110fc72085e673d761a05bc
Andrei Beloziorov <andreibeloziorove@Andreis-MacBook-Pro.local>
2025-08-15 21:24:36 +0300

    Add CartItem entity and repository for managing shopping cart items

---
+++ src/main/java/com/andreibel/ecomapplication/model/CartItem.java
@@ -0,0 +1,43 @@
+package com.andreibel.ecomapplication.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @NotNull
+    private Integer quantity;
+    @NotNull
+    private BigDecimal totalPrice;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
